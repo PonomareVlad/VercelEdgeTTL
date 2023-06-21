d0afc1a54fe751863b9182380b46d5a4fe5e04d0
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-06-22 03:47:09 +0500

    Invocations test

--- api/index.mjs
+++ api/index.mjs
@@ -1,8 +1,10 @@
 export const config = {runtime: "edge"}
 
-export default async ({url, body, headers}) => new Response(JSON.stringify({
-    query: Object.fromEntries(new URL(url).searchParams.entries()),
-    headers: Object.fromEntries(headers.entries()),
-    body: await new Response(body).text(),
-    env: process.env,
+globalThis.invocations ??= 0
+
+const born = new Date().toISOString()
+
+export default async () => new Response(JSON.stringify({
+    invocations: globalThis.invocations += 1,
+    born
 }))
